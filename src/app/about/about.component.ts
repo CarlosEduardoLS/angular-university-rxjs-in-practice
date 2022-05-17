5a55933e1cbdacc45787d0a4694334e53c688133
Carlos Eduardo <carloseduardo.aulas@gmail.com>
2022-05-16 23:19:49 -0300

    FEAT: Understanding error, completion and subscription of rxjs

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -12,11 +12,17 @@ export class AboutComponent implements OnInit {
 
     const timer$ = timer(3000, 1000);
 
-    interval$.subscribe((val) => console.log("Stream 1 " + val));
+    const sub = interval$.subscribe((val) => console.log("Stream 1 " + val));
     timer$.subscribe((val) => console.log("Stream 2 " + val));
+
+    setTimeout(() => sub.unsubscribe(), 5000);
 
     const click$ = fromEvent(document, "click");
 
-    click$.subscribe((event) => console.log(event));
+    click$.subscribe(
+      (event) => console.log(event),
+      (error) => console.log(error),
+      () => console.log("completed")
+    );
   }
 }
