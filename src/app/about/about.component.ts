ceb887a137367abf010c4c80a1925d375de1cd03
Carlos Eduardo <carloseduardo.aulas@gmail.com>
2022-05-16 23:15:27 -0300

    FEAT: Using interval, timer and fromEvent

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,40 +1,22 @@
-import {Component, OnInit, ViewEncapsulation} from '@angular/core';
-import {
-    concat,
-    fromEvent,
-    interval,
-    noop,
-    observable,
-    Observable,
-    of,
-    timer,
-    merge,
-    Subject,
-    BehaviorSubject,
-    AsyncSubject,
-    ReplaySubject
-} from 'rxjs';
-import {delayWhen, filter, map, take, timeout} from 'rxjs/operators';
-import {createHttpObservable} from '../common/util';
-
+import { Component, OnInit } from "@angular/core";
+import { fromEvent, interval, timer } from "rxjs";
 
 @Component({
-    selector: 'about',
-    templateUrl: './about.component.html',
-    styleUrls: ['./about.component.css']
+  selector: "about",
+  templateUrl: "./about.component.html",
+  styleUrls: ["./about.component.css"],
 })
 export class AboutComponent implements OnInit {
+  ngOnInit() {
+    const interval$ = interval(1000);
 
-    ngOnInit() {
+    const timer$ = timer(3000, 1000);
 
+    interval$.subscribe((val) => console.log("Stream 1 " + val));
+    timer$.subscribe((val) => console.log("Stream 2 " + val));
 
-    }
+    const click$ = fromEvent(document, "click");
 
-
+    click$.subscribe((event) => console.log(event));
+  }
 }
-
-
-
-
-
-
