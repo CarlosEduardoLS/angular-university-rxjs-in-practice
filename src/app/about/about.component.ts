d3fe81094b81bb68c4923ef2a145a7dc1b093c35
Carlos Eduardo <carloseduardo.aulas@gmail.com>
2022-05-29 20:52:37 -0300

    FEAT: Understanding the merge Observable

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
-import { concat, of } from "rxjs";
+import { concat, interval, merge, of } from "rxjs";
+import { map } from "rxjs/operators";
 
 @Component({
   selector: "about",
@@ -8,10 +9,9 @@ import { concat, of } from "rxjs";
 })
 export class AboutComponent implements OnInit {
   ngOnInit() {
-    const source1$ = of(1, 2, 3);
-    const source2$ = of(4, 5, 6);
-    const source3$ = of(7, 8, 9);
-    const result$ = concat(source1$, source2$, source3$);
+    const interval1$ = interval(1000);
+    const interval2$ = interval1$.pipe(map((val) => val * 10));
+    const result$ = merge(interval1$, interval2$);
     result$.subscribe(console.log);
   }
 }
