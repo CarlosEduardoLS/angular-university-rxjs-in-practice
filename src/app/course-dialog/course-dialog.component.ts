ca8256b60a2f3a1dd96df35c118bb3f235dd728b
Carlos Eduardo <carloseduardo.aulas@gmail.com>
2022-05-29 19:37:48 -0300

    FEAT: Form Draft Pre-Save Example

--- src/app/course-dialog/course-dialog.component.ts
+++ src/app/course-dialog/course-dialog.component.ts
@@ -1,63 +1,77 @@
-import {AfterViewInit, Component, ElementRef, Inject, OnInit, ViewChild, ViewEncapsulation} from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  Inject,
+  OnInit,
+  ViewChild,
+} from "@angular/core";
 import { MAT_DIALOG_DATA, MatDialogRef } from "@angular/material/dialog";
-import {Course} from "../model/course";
-import {FormBuilder, Validators, FormGroup} from "@angular/forms";
-import * as moment from 'moment';
-import {fromEvent, noop} from 'rxjs';
-import {concatMap, distinctUntilChanged, exhaustMap, filter, mergeMap, tap} from 'rxjs/operators';
-import {fromPromise} from 'rxjs/internal-compatibility';
-import {Store} from '../common/store.service';
+import { Course } from "../model/course";
+import { FormBuilder, Validators, FormGroup } from "@angular/forms";
+import * as moment from "moment";
+import { Store } from "../common/store.service";
+import { filter } from "rxjs/operators";
+import { fromPromise } from "rxjs/internal-compatibility";
 
 @Component({
-    selector: 'course-dialog',
-    templateUrl: './course-dialog.component.html',
-    styleUrls: ['./course-dialog.component.css']
+  selector: "course-dialog",
+  templateUrl: "./course-dialog.component.html",
+  styleUrls: ["./course-dialog.component.css"],
 })
-export class CourseDialogComponent implements AfterViewInit {
+export class CourseDialogComponent implements AfterViewInit, OnInit {
+  form: FormGroup;
 
-    form: FormGroup;
+  course: Course;
 
-    course:Course;
+  @ViewChild("saveButton", { static: true }) saveButton: ElementRef;
 
-    @ViewChild('saveButton', { static: true }) saveButton: ElementRef;
+  @ViewChild("searchInput", { static: true }) searchInput: ElementRef;
 
-    @ViewChild('searchInput', { static: true }) searchInput : ElementRef;
+  constructor(
+    private fb: FormBuilder,
+    private dialogRef: MatDialogRef<CourseDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) course: Course,
+    private store: Store
+  ) {
+    this.course = course;
 
-    constructor(
-        private fb: FormBuilder,
-        private dialogRef: MatDialogRef<CourseDialogComponent>,
-        @Inject(MAT_DIALOG_DATA) course:Course,
-        private store:Store) {
+    this.form = fb.group({
+      description: [course.description, Validators.required],
+      category: [course.category, Validators.required],
+      releasedAt: [moment(), Validators.required],
+      longDescription: [course.longDescription, Validators.required],
+    });
+  }
 
-        this.course = course;
+  ngAfterViewInit() {}
 
-        this.form = fb.group({
-            description: [course.description, Validators.required],
-            category: [course.category, Validators.required],
-            releasedAt: [moment(), Validators.required],
-            longDescription: [course.longDescription,Validators.required]
-        });
+  ngOnInit(): void {
+    this.form.valueChanges
+      .pipe(filter(() => this.form.valid))
+      .subscribe((changes) => {
+        const saveCourses$ = fromPromise(
+          fetch(`/api/courses/${this.course.id}`, {
+            method: "PUT",
+            body: JSON.stringify(changes),
+            headers: {
+              "content-type": "application/json",
+            },
+          })
+        );
 
-    }
+        saveCourses$.subscribe();
+      });
+  }
 
-    ngAfterViewInit() {
+  save() {
+    this.store.saveCourse(this.course.id, this.form.value).subscribe(
+      () => this.close(),
+      (err) => console.log("Error saving course", err)
+    );
+  }
 
-    }
-
-    save() {
-        this.store.saveCourse(this.course.id, this.form.value)
-            .subscribe(
-                () => this.close(),
-                err => console.log("Error saving course", err)
-            );
-    }
-
-
-
-
-    close() {
-        this.dialogRef.close();
-    }
-
-
+  close() {
+    this.dialogRef.close();
+  }
 }
